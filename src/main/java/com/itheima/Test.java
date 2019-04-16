b6cf43aa0a67c810959216dbb00b7858458abd28
Kevin <1318595887@qq.com>
2019-04-16 14:41:33 +0800

    添加全局过滤文件

---
+++ src/main/java/com/itheima/Test.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * Package: com.itheima
+ * Description： TODO
+ * Author: Kevin
+ * Date: Created in 2019/4/16 下午2:40
+ * Company: itheima
+ * Copyright: Copyright (c) 2019
+ * Version: TODO
+ * Modified By:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("添加全局过滤文件");
+    }
+}
